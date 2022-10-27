562d8972de49eb890757caaa8e0e1c4bbe07ead2
whddla <whddla8912@gmail.com>
2022-10-28 01:37:20 +0900

    Lv3 동적계획법 문제

--- 프로그래머스/Lv3/dynamic.py
+++ 프로그래머스/Lv3/dynamic.py
@@ -9,13 +9,39 @@
 # triangle	result
 # [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]	30
 
-def solution(triangle):
-    for i in triangle:
-        cnt = 0
-        for j in i:
-            if i[j]:
-                cnt += 1
-    return cnt
+# def solution(triangle):
+#     cnt = 0
+#     result = []
+#     for i in triangle:
+#         num = 0
+#         for j in i:
+#             num += j
+#             print(num)
+#         cnt += 1
+#         result.append(num)
+#     return result
 
-                
+# 출처 HUGO RYU님
+
+def solution(triangle):
+    answer = triangle[0][0]
+    if len(triangle) == 2:
+        answer += max(triangle[1])
+    else:
+        l = len(triangle)
+        dp = [0] * l
+        dp[0] = triangle[0]
+        for i in range(1, l):  # 층 수, level = i+1
+            dp[i] = [0] * (i+1)
+            for j in range(i+1):
+                if j == 0:
+                    dp[i][j] = dp[i-1][j] + triangle[i][j]
+                elif j == i:
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                else:
+                    dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+        answer = max(dp[l-1])
+    return answer
+
+
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
